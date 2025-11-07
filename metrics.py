053d73d0b244ab6d047b0924fe9644cbeb07502b
Moos <moos@cwi.nl>
2025-11-07 14:07:56 +0100

    proportional clipped_negative_mean_difference() metric

--- metrics.py
+++ metrics.py
@@ -12,14 +12,18 @@ def mean_difference(acc_before, acc_after, target_idx: int):
     mean_delta_others = sum(delta_others) / len(delta_others)
     return delta_target - mean_delta_others
 
-def clipped_negative_mean_difference(acc_before, acc_after, target_idx):
+def clipped_negative_mean_difference(acc_before, acc_after, target_idx, proportional=True):
     """
     Calculate clipped mean unlearned metric.
 
     Similar to mean unlearned but clips negative differences to zero.
     """
-    delta_target = acc_before[target_idx] - acc_after[target_idx]
-    delta_others = [acc_before[i] - acc_after[i] for i in range(len(acc_before)) if i != target_idx]
+    if proportional:
+        delta_target = (acc_before[target_idx] - acc_after[target_idx]) / acc_before[target_idx]
+        delta_others = [(acc_before[i] - acc_after[i]) / acc_before[i] for i in range(len(acc_before)) if i != target_idx]
+    else:
+        delta_target = acc_before[target_idx] - acc_after[target_idx]
+        delta_others = [acc_before[i] - acc_after[i] for i in range(len(acc_before)) if i != target_idx]
     return max(0.0, delta_target) - sum(max(0.0, d) for d in delta_others) / len(delta_others)
 
 def max_difference(acc_before, acc_after, target_idx):
@@ -45,16 +49,28 @@ def min_difference(acc_before, acc_after, target_idx):
 if __name__ == "__main__":
     # Example usage
     import numpy as np
-    acc_before = np.array([0.8, 0.8, 0.85, 0.95])
-    acc_after = np.array([0.9, 0.75, 0.5, 0.9])
-    target_idx = 2
+    acc_before = np.array([0.5, 0.5, 0.5])
+    acc_after = np.array([0.25, 0, 0.25])
+    acc_before2 = np.array([1, 1, 1])
+    acc_after2 = np.array([0.5, 0, 0.5])
+    target_idx = 1
 
-    print("Unlearning index", target_idx)
-    print("Before:", acc_before)
-    print("After:", acc_after)
-    print("Delta:", acc_after - acc_before)
+    metric_1 = clipped_negative_mean_difference(acc_before, acc_after, target_idx)
+    metric_2 = clipped_negative_mean_difference(acc_before2, acc_after2, target_idx)
+    print(f"Clipped Mean Unlearned 1: {metric_1:.2f}")
+    print(f"Clipped Mean Unlearned 2: {metric_2:.2f}")
+    assert metric_1 == metric_2, "Metrics should be equal for proportional drops"
 
-    print(f"Mean Unlearned: {mean_difference(acc_before, acc_after, target_idx):.2f}")
-    print(f"Clipped Mean Unlearned: {clipped_negative_mean_difference(acc_before, acc_after, target_idx):.2f}")
-    print(f"Max Unlearned: {max_difference(acc_before, acc_after, target_idx):.2f}")
-    print(f"Min Unlearned: {min_difference(acc_before, acc_after, target_idx):.2f}")
+    # acc_before = np.array([0.8, 0.8, 0.85, 0.95])
+    # acc_after = np.array([0.9, 0.75, 0.5, 0.9])
+    # target_idx = 2
+
+    # print("Unlearning index", target_idx)
+    # print("Before:", acc_before)
+    # print("After:", acc_after)
+    # print("Delta:", acc_after - acc_before)
+
+    # print(f"Mean Unlearned: {mean_difference(acc_before, acc_after, target_idx):.2f}")
+    # print(f"Clipped Mean Unlearned: {clipped_mean_difference(acc_before, acc_after, target_idx):.2f}")
+    # print(f"Max Unlearned: {max_difference(acc_before, acc_after, target_idx):.2f}")
+    # print(f"Min Unlearned: {min_difference(acc_before, acc_after, target_idx):.2f}")
