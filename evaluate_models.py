32d7450dde304ab8ccd1e12e221768c751153f24
Simon <ilic@cwi.nl>
2025-11-12 10:18:17 +0100

    add other metrics

--- evaluate_models.py
+++ evaluate_models.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from tqdm import tqdm
-from cnn_surgery.utils.load_dataset import load_multi_stage_dataset
+from cnn_surgery.utils.load_dataset import load_multi_stage_dataset, load_dataset
 from cnn_surgery.utils.reconstruct_network import reconstruct_network
 from cnn_surgery.utils.evaluate_per_class_accuracy import evaluate_classifier, load_testset_data
 from cnn_surgery.utils.metrics import clipped_negative_mean_difference, min_difference, max_difference
@@ -10,46 +10,55 @@ from cnn_surgery.utils.metrics import clipped_negative_mean_difference, min_diff
 from unlearning import unlearn
 import os
 # experiment parameters
-N_MODELS = 100  # Number of models to evaluate
-TARGET_CLASS = 4
-DATASET = 'fashion_mnist'
+N_MODELS = 1000  # Number of models to evaluate
+TARGET_CLASS = 5
+DATASET = 'mnist'
 
 #unlearning parameters
 MAX_STEPS = 10000
-LR = 0.01
-EPS = 0.8
+LR = 0.1
+EPS = 0.9
 
 # CNN evaluation data
 x_test, y_test = load_testset_data(DATASET)
 
-data = load_multi_stage_dataset(dataset=DATASET)
-weights_train, accuracies_train, config_train = data['train']
-weights_val, accuracies_val, config_val = data['val']
+_, _, val_data = load_dataset(dataset=DATASET, metrics_file='metrics_merged_final.csv', load_class_acc=True)
+weights_val, metrics_val, config_val = val_data
 
-meta_network = pickle.load(open('meta_network.pkl', 'rb'))
+test_accuracies = np.array([m[0] for m in metrics_val])
+accuracies_val = metrics_val[:, -10:]
+print(test_accuracies[test_accuracies.argsort()[-N_MODELS:][::-1]])
+
+meta_network = pickle.load(open(f'meta_network_{DATASET}.pkl', 'rb'))
 meta_network.eval()
 
 metrics = []
-for MODEL_IDX in tqdm(range(-N_MODELS, 0)):
+for MODEL_IDX in tqdm(range(N_MODELS)):
     network = weights_val[MODEL_IDX]
     accuracy = accuracies_val[MODEL_IDX]
     config = config_val.iloc[MODEL_IDX]
 
+    if 0.0 in accuracy:
+        print("Skipping model with 0 accuracy in some class.")
+        continue
+
     edited_network = unlearn(network, meta_network, TARGET_CLASS,
-                             max_steps=MAX_STEPS, lr=LR, eps=EPS, l2_penalty=0.0).squeeze(0).detach()
+                             max_steps=MAX_STEPS, lr=LR, eps=EPS, l2_penalty=1e-6).squeeze(0).detach()
     model = reconstruct_network(edited_network.numpy(), config['config.activation'])
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     acc_after = evaluate_classifier(model, x_test, y_test)
     total_accuracy, accuracy_after = acc_after
-    metrics.append(clipped_negative_mean_difference(accuracy, accuracy_after, TARGET_CLASS))
+    metrics.append(clipped_negative_mean_difference(accuracy, accuracy_after, TARGET_CLASS, proportional=False))
     
     out_file = 'evaluation_results.csv'
     row = pd.DataFrame([{
-        'model_idx': len(weights_val) - MODEL_IDX,
-        'original_accuracy': accuracy,
+        'model_idx': MODEL_IDX,
+        'original_accuracy': list(accuracy),
         'accuracy_after': accuracy_after,
         'total_accuracy': total_accuracy,
         'clipped_negative_mean_difference': metrics[-1],
+        'max_difference': max_difference(accuracy, accuracy_after, TARGET_CLASS),
+        'unlearned_target_difference': target_difference(accuracy, accuracy_after, TARGET_CLASS),
         'target_class': TARGET_CLASS,
         'lr': LR,
         'eps': EPS,
