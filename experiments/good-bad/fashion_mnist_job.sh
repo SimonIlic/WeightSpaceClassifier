81dc2aaf4dcec8d0fa181b1b19bd320046cf0996
Moos <moos@cwi.nl>
2025-11-26 14:11:50 +0100

    eval models fashion mnist bash script

---
+++ experiments/good-bad/fashion_mnist_job.sh
@@ -0,0 +1,17 @@
+DATASET=fashion_mnist
+
+for i in {0..4}; do
+    for j in {0..9}; do
+        python ~/Desktop/snijzaal/WeightSpaceClassifier/evaluate_models.py \
+            --n-models 100 \
+            --target-class $j \
+            --dataset $DATASET \
+            --output-file ~/Desktop/snijzaal/WeightSpaceClassifier/experiments/good-bad/${DATASET}_eval_results_meta_${i}_class_${j}.csv \
+            --max-steps 1000 \
+            --lr 0.3 \
+            --stop-threshold 0.5 \
+            --stopping-criterium acc_pred \
+            --loss-fn boost \
+            --meta-network-path ~/Desktop/snijzaal/WeightSpaceClassifier/models/good_bad_experiment_2/${DATASET}_metanetwork_$i.pt
+    done
+done
