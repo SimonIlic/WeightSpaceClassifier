653d0e1c3067b33fb5187ef26d4f59dfe5b78744
Simon <ilic@cwi.nl>
2025-12-10 13:44:58 +0100

    define per class experiment

---
+++ per_class_evaluation.sh
@@ -0,0 +1,12 @@
+dataset='fashion_mnist'
+for cls in 0 1 2 3 4 5 6 7 8 9; do                                                                                                                                              
+python evaluate_models.py \
+    -c=$cls \
+    -d=$dataset \
+    --stopping-criterium='acc_pred' \
+    --max-steps=2000 \
+    --loss-fn='simple' \
+    --stop-threshold=0.3 \
+    --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
+    -o="${dataset}_class_evaluation.csv"
+done
