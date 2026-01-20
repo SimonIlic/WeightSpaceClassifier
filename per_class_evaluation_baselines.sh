225d852111b18b704ad8094cd6b48341d180fedb
Simon <ilic@cwi.nl>
2026-01-20 14:52:36 +0100

    experiment

--- per_class_evaluation_baselines.sh
+++ per_class_evaluation_baselines.sh
@@ -1,13 +1,12 @@
-dataset='mnist'
+dataset='fashion_mnist'
 for cls in 0 1 2 3 4 5 6 7 8 9; do                                                                                                                                              
 python src/cnn_surgery/evaluate_models.py \
     -c=$cls \
     -d=$dataset \
-    -n=200 \
-    --stopping-criterium='acc_pred' \
+    --stopping-criterium='acc_pred_relative' \
     --max-steps=2000 \
-    --loss-fn='simple' \
-    --stop-threshold=0.3 \
+    --loss-fn='boost' \
+    --stop-threshold=0.4 \
     --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
-    -o="${dataset}_class_evaluation_baselines.csv"
+    -o="${dataset}_class_evaluation_baselines_relative.csv"
 done
