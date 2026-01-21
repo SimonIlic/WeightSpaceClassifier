3480106844a2e3abd12aab0fad80aba9613eefce
Moos <moos@cwi.nl>
2026-01-21 19:15:50 +0100

    start up the process anew every 100 modelz

--- per_class_evaluation_baselines.sh
+++ per_class_evaluation_baselines.sh
@@ -1,12 +1,17 @@
 dataset='fashion_mnist'
-for cls in 0 1 2 3 4 5 6 7 8 9; do                                                                                                                                              
-python src/cnn_surgery/evaluate_models.py \
-    -c=$cls \
-    -d=$dataset \
-    --stopping-criterium='acc_pred_relative' \
-    --max-steps=2000 \
-    --loss-fn='boost' \
-    --stop-threshold=0.4 \
-    --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
-    -o="${dataset}_class_evaluation_baselines_relative_2.csv"
+for cls in 0 1 2 3 4 5 6 7 8 9; do 
+    for idx in $(seq 0 100 7500); do
+        python src/cnn_surgery/evaluate_models.py \
+            -c=$cls \
+            -d=$dataset \
+            --start-idx=$idx \
+            -n=100 \
+            --weights-set='test' \
+            --stopping-criterium='acc_pred_relative' \
+            --max-steps=2000 \
+            --loss-fn='boost' \
+            --stop-threshold=0.4 \
+            --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
+            -o="${dataset}_class_evaluation_baselines_relative_3.csv"
+    done
 done
