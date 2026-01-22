7890cf98290429dc1af1f46628010569e7cb8c10
Simon <ilic@cwi.nl>
2026-01-22 16:53:55 +0100

    update experiment

--- per_class_evaluation_baselines.sh
+++ per_class_evaluation_baselines.sh
@@ -1,9 +1,9 @@
-dataset='fashion_mnist'
+dataset='mnist'
 group_size=500
 for cls in 0 1 2 3 4 5 6 7 8 9; do 
     for idx in $(seq 0 $group_size 7500); do
         echo "Evaluating class $cls, until index $((idx + group_size))"
-        python src/cnn_surgery/evaluate_models.py \
+        KERAS_BACKEND=torch python src/cnn_surgery/evaluate_models.py \
             -c=$cls \
             -d=$dataset \
             --start-idx=$idx \
@@ -14,6 +14,6 @@ for cls in 0 1 2 3 4 5 6 7 8 9; do
             --loss-fn='boost' \
             --stop-threshold=0.4 \
             --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
-            -o="${dataset}_class_evaluation_baselines_relative_3.csv"
+            -o="${dataset}_class_evaluation_baselines_20260121.csv"
     done
 done
