c9b25cc02079c4548e64dd470699727552cc7c4b
Simon <ilic@cwi.nl>
2026-01-27 21:05:47 +0100

    improve loss experimetn

--- improve_loss_test.sh
+++ improve_loss_test.sh
@@ -1,7 +1,7 @@
-dataset='fashion_mnist'
+dataset='mnist'
 group_size=100
-for cls in 0 1 2 3 4; do 
-    for idx in $(seq 0 $group_size 200); do
+for cls in 0 1 2 3 4 5 6 7 8 9 ; do 
+    for idx in $(seq 0 $group_size 1000); do
         echo "Evaluating class $cls, until index $((idx + group_size))"
         KERAS_BACKEND=torch python src/cnn_surgery/evaluate_models.py \
             -c=$cls \
@@ -13,7 +13,7 @@ for cls in 0 1 2 3 4; do
             --loss-fn='improve' \
             --stop-threshold=0.9 \
             --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
-            -o="${dataset}_improve_loss_test.csv" \
-            --weights-set='val'
+            -o="${dataset}_improve_loss.csv" \
+            --weights-set='test'
     done
 done
