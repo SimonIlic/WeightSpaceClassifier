c01f91de330e88675d16a1bfa55102276003e41c
Moos <moos@cwi.nl>
2026-06-23 15:14:49 +0200

    dont remember what this was for

--- improve_loss_test.sh
+++ improve_loss_test.sh
@@ -1,19 +1,19 @@
-dataset='mnist'
-group_size=100
-for cls in 0 1 2 3 4 5 6 7 8 9 ; do 
-    for idx in $(seq 0 $group_size 1000); do
-        echo "Evaluating class $cls, until index $((idx + group_size))"
-        KERAS_BACKEND=torch python src/cnn_surgery/evaluate_models.py \
-            -c=$cls \
-            -d=$dataset \
-            --start-idx=$idx \
-            -n=$group_size \
-            --stopping-criterium='acc_pred_improve' \
-            --max-steps=1500 \
-            --loss-fn='improve' \
-            --stop-threshold=0.9 \
-            --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
-            -o="${dataset}_improve_loss.csv" \
-            --weights-set='test'
+dataset='svhn_cropped'
+group_size=500
+idx=0
+for cls in 2 3 4 5 6 7 8 9 ; do 
+    echo "Evaluating class $cls, until index $((idx + group_size))"
+    python src/cnn_surgery/evaluate_models.py \
+        -c=$cls \
+        -d=$dataset \
+        --start-idx=$idx \
+        -n=$group_size \
+        --stopping-criterium='acc_pred_improve' \
+        --max-steps=1500 \
+        --loss-fn='improve' \
+        --stop-threshold=0.9 \
+        --meta-network-path="metanetworks/meta_network_${dataset}_0.pkl" \
+        -o="${dataset}_improve_loss.csv" \
+        --weights-set='test'
     done
 done
